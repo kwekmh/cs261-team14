b2391ccf55d96ab8a2a6924e37abfe6a9d4a072f
OMoynes <otmoynes@gmail.com>
2017-02-26 15:37:18 +0000

    Implement alert sound (toggle through pulse(count, sound) method)

--- src/main/resources/static/scripts/pulse.js
+++ src/main/resources/static/scripts/pulse.js
@@ -1,26 +1,31 @@
 function pulsein() {
-	$('#alertIcon').animate({
-	        width: 30, height: 30
-	    });
+    $('#alertIcon').animate({
+        width: 30, height: 30
+    });
 }
 
 function pulseout() {
-	$('#alertIcon').animate({
-	        width: 20, height: 20
-	    });
+    $('#alertIcon').animate({
+        width: 20, height: 20
+    });
 }
 
-function pulse(count) {
-	$("#alertIcon").show();
-	for (var i = 0; i < count; i++) {
-		pulsein();
-		pulseout();
-	}
-	$("#alertIcon").fadeOut();
+function pulse(count, sound) {
+    $("#alertIcon").show();
+    if (sound=='on') {
+        audio.play();
+    }
+    for (var i = 0; i < count; i++) {
+        pulsein();
+        pulseout();
+    }
+    $("#alertIcon").fadeOut();
 }
 
 var tempPulser = document.getElementById("streamHook");
 
+var audio = new Audio('../sounds/alert1.mp3');
+
 tempPulser.onclick = function() {
-    pulse(3);
+    pulse(3,'on');
 };
